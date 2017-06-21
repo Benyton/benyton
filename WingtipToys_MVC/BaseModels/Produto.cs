60ac1e302c76a17b2f083c5c275476ee15c2aa15
1528944 <1528944@045LAB04031>
2017-06-20 21:23:48 -0300

    Erro metade da aula...

---
+++ WingtipToys_MVC/BaseModels/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BaseModels
+{
+    class Produto
+    {
+        [Required]
+        [StringLength(20)]
+        public string Nome { get; set; }
+        
+        public string Descricao { get; set; }
+
+        [Required]
+        public string Ativo { get; set; }
+
+        // -- Relacionamento Categoria --> Produto
+        [ForeignKey("_Categoria")]
+        public int CategoriaID { get; set; }
+
+        public virtual Categoria _Categoria { get; set; }
+    }
+}
